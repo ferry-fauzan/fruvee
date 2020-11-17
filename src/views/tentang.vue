e7db2068bb126448617380b5b98e77d88e0e6884
hdinjos <hdinjos@gmail.com>
2020-11-17 17:01:16 +0700

    Fruvee. About page

--- src/views/tentang.vue
+++ src/views/tentang.vue
@@ -1,68 +1,28 @@
 <template>
-<div>
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container">
-        <a class="navbar-brand" href="#">Hdinjos</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav ml-auto">
-            <a class="nav-item nav-link active" href="#"
-              >Home <span class="sr-only">(current)</span></a
-            >
-            <a class="nav-item nav-link" href="#">Pricing</a>
-            <a class="nav-item nav-link" href="#">Features</a>
-            <a class="nav-item nav-link" href="#">About</a>
-            <a class="nav-item btn btn-warning mybtn" href="#">Join Us</a>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4 font-weight-light">
-          Get work done <span>faster</span> <br />
-          and <span>better</span> with us
-        </h1>
-        <a class="btn btn-warning mybtn" href="">Our Work</a>
-      </div>
-    </div>
-</div>
+    <v-card color="#f5f5f5">
+    <v-row class="mt-16 mb-10">
+      <v-col cols="12" md="6" class="my-10">
+        <h1 class="text-justify mt-5 mx-5">Fruvee</h1>
+        <p class="text-justify my-2 mx-5">Fruvee adalah layanan penjualan buah dan sayuran yang menghubungkan antara penjual dan pembeli secara langsung. Dengan Fruvee cara kita dengan mudah untuk mendapatkan akses hidup sehat. Fruvee cara mudah hidup sehat dengan hanya sekali klik!!!</p>
+      </v-col>
+      <v-col cols="12" md="6" class="my-10">
+        <!-- <v-card height="250"> -->
+         <v-img
+        src="../assets/ab1.jpg"
+        height="250"
+                      
+        contain>
+      </v-img>
+
+        <!-- </v-card> -->
+      </v-col>
+    </v-row>
+
+    </v-card>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
-<style scoped>
-.navbar {
-    position: relative;
-    z-index: 1;
-}
-
-.navbar-brand {
-    font-family: Viga;
-    font-size: 32px;
-}
-
-.jumbotron {
-    background-image: url("../assets/img3.jpg");
-    background-size: cover;
-    height: 540px;
-    text-align: center;
-    position: relative;
-}
-
-
+<style>
+img{
+    position: right;
+} 
 </style>
