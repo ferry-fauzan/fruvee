4d5cce1edd2d7a95a875b8f3ac1ce1d0308dae10
hdinjos <hdinjos@gmail.com>
2020-11-17 17:50:12 +0700

    edit tentang

--- src/views/tentang.vue
+++ src/views/tentang.vue
@@ -1,28 +1,34 @@
 <template>
-    <v-card color="#f5f5f5">
-    <v-row class="mt-16 mb-10">
-      <v-col cols="12" md="6" class="my-10">
+  <!-- <v-card color="#f5f5f5"> -->
+  <v-row justify="center" align="center" class="mt-16">
+    <!-- <v-col cols="12" md="6" class="my-10"> -->
+    <v-col cols="12" md="4">
+      <v-img src="../assets/ab1.jpg" height="250" contain> </v-img>
+    </v-col>
+    <v-col cols="10" md="4">
+      <v-row justify="start">
         <h1 class="text-justify mt-5 mx-5">Fruvee</h1>
-        <p class="text-justify my-2 mx-5">Fruvee adalah layanan penjualan buah dan sayuran yang menghubungkan antara penjual dan pembeli secara langsung. Dengan Fruvee cara kita dengan mudah untuk mendapatkan akses hidup sehat. Fruvee cara mudah hidup sehat dengan hanya sekali klik!!!</p>
-      </v-col>
-      <v-col cols="12" md="6" class="my-10">
-        <!-- <v-card height="250"> -->
-         <v-img
-        src="../assets/ab1.jpg"
-        height="250"
-                      
-        contain>
-      </v-img>
+        <p class="text-justify my-2 mx-5">
+          Fruvee adalah layanan penjualan buah dan sayuran yang menghubungkan
+          antara penjual dan pembeli secara langsung. Dengan Fruvee cara kita
+          dengan mudah untuk mendapatkan akses hidup sehat. Fruvee cara mudah
+          hidup sehat dengan hanya sekali klik!!!
+        </p>
+      </v-row>
+    </v-col>
+    <!-- </v-co1l> -->
+    <!-- <v-col cols="12" md="6" class="my-10"> -->
+    <!-- <v-card height="250"> -->
 
-        <!-- </v-card> -->
-      </v-col>
-    </v-row>
+    <!-- </v-card> -->
+    <!-- </v-col> -->
+  </v-row>
 
-    </v-card>
+  <!-- </v-card> -->
 </template>
 
 <style>
-img{
-    position: right;
-} 
+img {
+  position: right;
+}
 </style>
