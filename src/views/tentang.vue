33ac18f5c9b78877edfcfeaee542d69bd8fef3a7
hdinjos <hdinjos@gmail.com>
2020-11-20 13:07:12 +0700

    upadate tentang

--- src/views/tentang.vue
+++ src/views/tentang.vue
@@ -1,19 +1,32 @@
 <template>
-  <!-- <v-card color="#f5f5f5"> -->
-  <v-row justify="center" align="center" class="mt-16">
+<div>
+<v-col>
+  <v-row justify="center" style="margin: 200px 0" >
+    <v-img src="../assets/f2.png" max-height="150" max-width="270" ></v-img>
+  </v-row>
+</v-col>
+
+
+  <v-card style="padding: 200px 0" color="#f5f5f5">
+  <v-row justify="center" align="center">
     <!-- <v-col cols="12" md="6" class="my-10"> -->
-    <v-col cols="12" md="4">
-      <v-img src="../assets/ab1.jpg" height="250" contain> </v-img>
+    <v-col cols="" md="4">
+      <div class="circle">
+        <v-img src="../assets/ab1.jpg" height="250" contain> </v-img>
+      </div>
     </v-col>
-    <v-col cols="10" md="4">
-      <v-row justify="start">
-        <h1 class="text-justify mt-5 mx-5">Fruvee</h1>
-        <p class="text-justify my-2 mx-5">
+    <v-col cols="5" md="4">
+      <v-row justify="left">
+        <h3 class="text-center mt-3 mx-1 padding-right cf">Tentang Kami</h3>
+        <p class="text-left my-1 mx-1 tf">
           Fruvee adalah layanan penjualan buah dan sayuran yang menghubungkan
           antara penjual dan pembeli secara langsung. Dengan Fruvee cara kita
           dengan mudah untuk mendapatkan akses hidup sehat. Fruvee cara mudah
           hidup sehat dengan hanya sekali klik!!!
+          dalah layanan penjualan buah dan sayuran yang menghubungkan
+          antara penjual dan pembeli secara langsung.
         </p>
+        <v-btn class="mt-5" depressed color="primary" href="https://wa.me/6285641723138" cfr>contact us</v-btn>
       </v-row>
     </v-col>
     <!-- </v-co1l> -->
@@ -24,11 +37,36 @@
     <!-- </v-col> -->
   </v-row>
 
-  <!-- </v-card> -->
+  </v-card>
+
+  <!-- <section>
+    <v-col>
+      <video src="https://www.youtube.com/watch?v=qnXcB5L97MI" width="100%" height="100%">
+        <source src="https://www.youtube.com/watch?v=qnXcB5L97MI" type="video / mp4">
+      </video>
+    </v-col>
+  </section> -->
+  </div>
 </template>
 
 <style>
-img {
-  position: right;
+@import url("https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Cabin:wght@700&display=swap");
+
+.cf {
+  font-family: "Cabin", sans-serif;
+  color: #008f28;
+  font-size: 20pt;
+}
+.cff {
+  font-family: "Cabin", sans-serif;
+  font-size: 20pt;
+  color: #008f28;
+}
+.tf {
+  color: #343A40;
+}
+
+.cfr{
+  font-family: "Cabin", sans-serif;
 }
 </style>
