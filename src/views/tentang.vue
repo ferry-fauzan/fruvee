b573c2f4faf9a588bf6831fa96d9c23688433b3a
hdinjos <hdinjos@gmail.com>
2020-11-20 13:16:56 +0700

    upadate tentang

--- src/views/tentang.vue
+++ src/views/tentang.vue
@@ -47,7 +47,7 @@
     </v-col>
   </section> -->
   </div>
-</template>
+</template>mamam dulu
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Cabin:wght@700&display=swap");
