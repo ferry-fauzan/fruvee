d31e1d744e51434326e28039e5d396ad63cf96fd
hdinjos <hdinjos@gmail.com>
2020-11-20 13:57:03 +0700

    update tentang

--- src/views/tentang.vue
+++ src/views/tentang.vue
@@ -2,6 +2,7 @@
 <div>
 <v-col>
   <v-row justify="center" style="margin: 200px 0" >
+            <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/nLeiVZ9BDKA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
     <v-img src="../assets/f2.png" max-height="150" max-width="270" ></v-img>
   </v-row>
 </v-col>
@@ -10,15 +11,21 @@
   <v-card style="padding: 200px 0" color="#f5f5f5">
   <v-row justify="center" align="center">
     <!-- <v-col cols="12" md="6" class="my-10"> -->
+        <v-col cols="12" justify="center" align="center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/nLeiVZ9BDKA" frameborder="0" allow="accelerometer; autoplay; clipboard-te;wri encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </v-col>
     <v-col cols="" md="4">
       <div class="circle">
-        <v-img src="../assets/ab1.jpg" height="250" contain> </v-img>
+
+        <!-- <v-img src="../assets/ab1.jpg" height="250" contain> </v-img> -->
       </div>
     </v-col>
-    <v-col cols="5" md="4">
-      <v-row justify="left">
+    <!-- <v-col cols="" md="4"> -->
+      <v-container>
+      <v-row justify="center">
         <h3 class="text-center mt-3 mx-1 padding-right cf">Tentang Kami</h3>
-        <p class="text-left my-1 mx-1 tf">
+        <p class="text-center lemy-1 mx-1 tf">
           Fruvee adalah layanan penjualan buah dan sayuran yang menghubungkan
           antara penjual dan pembeli secara langsung. Dengan Fruvee cara kita
           dengan mudah untuk mendapatkan akses hidup sehat. Fruvee cara mudah
@@ -28,7 +35,9 @@
         </p>
         <v-btn class="mt-5" depressed color="primary" href="https://wa.me/6285641723138" cfr>contact us</v-btn>
       </v-row>
-    </v-col>
+
+      </v-container>
+    <!-- </v-col> -->
     <!-- </v-co1l> -->
     <!-- <v-col cols="12" md="6" class="my-10"> -->
     <!-- <v-card height="250"> -->
